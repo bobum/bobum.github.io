0e76619b16dd40d47d8bd58c78a0305394379db5
bobum <scott@davisplanet.com>
2025-08-08 16:41:19 -0400

    Delete _plugins directory

--- _plugins/reading_time.rb
+++
@@ -1,9 +0,0 @@
-module Jekyll
-  module ReadingTimeFilter
-    def reading_time(input)
-      input.to_s.split.size.fdiv(180).ceil.to_s + " min read"
-    end
-  end
-end
-
-Liquid::Template.register_filter(Jekyll::ReadingTimeFilter)
